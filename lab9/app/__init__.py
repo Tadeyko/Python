314a7a6962a8e54160c98c29df2b0852ad8aa974
tadey09 <tadey7777@gmail.com>
2024-01-17 00:53:14 +0200

    lab9

---
+++ lab9/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.secret_key = b"secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+login_manager.init_app(app)
+
+
+
+from app import views
---
+++ lab9/app/models.py
@@ -0,0 +1,26 @@
+from app import db, login_manager
+from flask_login import UserMixin
+from datetime import datetime
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+    description = db.Column(db.String(300))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    about_me = db.Column(db.String(140))  
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+    
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(int(user_id))
+

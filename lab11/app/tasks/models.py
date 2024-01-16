0d49a6f19cf935a3fbc1e560ece8e720df1e264c
tadey09 <tadey7777@gmail.com>
2024-01-17 01:01:20 +0200

    lab11

---
+++ lab11/app/tasks/models.py
@@ -0,0 +1,7 @@
+from extensions import db
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+    description = db.Column(db.String(300))
